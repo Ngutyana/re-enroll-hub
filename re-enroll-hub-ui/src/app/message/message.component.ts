dc64791304bf94ce94cf2326fb496265eb6d49d4
Unathi  Ngutyana <ungutyana@ccint.net>
2023-10-22 21:37:07 +0200

    updates commit

--- re-enroll-hub-ui/src/app/message/message.component.ts
+++ re-enroll-hub-ui/src/app/message/message.component.ts
@@ -16,13 +16,11 @@ export class MessageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let m1 = new Message('I love you', 'Athi', new Date())
-    let m2 = new Message('Where are you', 'Slii', new Date())
-    let m3 = new Message('Not cool at all njayam', 'Unknown', new Date())
-    let m4 = new Message('Sleep over tonight?', 'Admin', new Date())
-    let m5 = new Message('Your application was rejected, fraud detected.', 'Sithole', new Date())
+    let m1 = new Message('Display Message from Admin', 'Yonela', new Date())
+    let m2 = new Message('Can be multiple messages', 'Eyethu', new Date())
+    let m3 = new Message('Will show who updated the messages and when', 'Sihle', new Date())
 
-    this.messages.push(m1, m2, m3, m4, m5);
+    this.messages.push(m1, m2, m3);
     if(!this.isSignedIn) {
       this.router.navigate([`login`]);
     }
